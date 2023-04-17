c96f8deefe68569b75f15d63bb4b3a65e9322b8c
Vishal <vishalmishrasep491@gmail.com>
2023-04-18 02:36:21 +0530

    Settled for now. Done!

--- Rock_Paper_Scissors/app.js
+++ Rock_Paper_Scissors/app.js
@@ -1,5 +1,5 @@
-const userScore = 0;
-const computerScore = 0;
+let userScore = 0;
+let computerScore = 0;
 const userScore_span = document.getElementById("user-score");
 const computerScore_span = document.getElementById("computer-score"); 
 //So the underscore is basically here to completely differentiate between the
@@ -9,7 +9,7 @@ const computerScore_span = document.getElementById("computer-score");
 // in Google Tag Manager to scrape content directly from your Website HTML
 // markup.(Basically here it helps to be identified as the span tag.)
 const scoreBoard_div = document.querySelector(".score-board");
-const result_div = document.querySelector(".result");
+const result_p = document.querySelector(".result > p");
 const rock_div = document.getElementById("r");
 const paper_div = document.getElementById("p");
 const scissors_div = document.getElementById("s");
@@ -40,17 +40,55 @@ function getComputerChoice(){
     const randomNumber = Math.floor(Math.random()*3);
     return choices[randomNumber];
 }
-
-function win(){
-    console.log("WIN")
+// The innerHTML property allows you to manipulate the HTML content of an element.
+function convertToWord(letter){
+    if(letter==="r") return "Rock";
+    if(letter==="p") return "Paper";
+    return "Scissors";
 }
 
-function lose(){
-    console.log("LOST")
+function win(userChoice, computerChoice){
+    const smallUserWord = "(user)".fontsize(3).sub();
+    const smallCompWord = "(comp)".fontsize(3).sub();
+    const userChoice_div = document.getElementById(userChoice);
+    // console.log("WIN")
+    userScore++;
+    userScore_span.innerHTML = userScore;
+    computerScore_span.innerHTML = computerScore;
+    // console.log(user)
+    // console.log(computer)
+    // result_p.innerHTML = convertToWord(userChoice) + " beats " + convertToWord(computerChoice) + ". You win! "; // CS-5 basically
+    result_p.innerHTML = `${convertToWord(userChoice)}${smallUserWord} beats ${convertToWord(computerChoice)}${smallCompWord}...You win!`; // CS-6 functions
+    // Addition of class green glow on whichever user clicked.
+    userChoice_div.classList.add('green-glow'); 
+    // setTimeout(function() {document.getElementById(userChoice).classList.remove('green-glow')}, 300); 
+    // Above line can also be written like
+    setTimeout(() => document.getElementById(userChoice).classList.remove('green-glow'), 300);   
+}
+// setTimeout takes two arguments 1st is the function and 2nd is the time in milliseconds of how long it should go.
+// setTimeout(function() {console.log("hello")}, 3000);//3000 milliseconds
+
+function lose(userChoice, computerChoice){
+    const smallUserWord = "(user)".fontsize(3).sub();
+    const smallCompWord = "(comp)".fontsize(3).sub();
+    const userChoice_div = document.getElementById(userChoice);
+    // console.log("LOST")
+    computerScore++;
+    userScore_span.innerHTML = userScore;
+    computerScore_span.innerHTML = computerScore;
+    result_p.innerHTML = `${convertToWord(userChoice)}${smallUserWord} loses to ${convertToWord(computerChoice)}${smallCompWord}...You lost!`;
+    userChoice_div.classList.add('red-glow'); 
+    setTimeout(() => document.getElementById(userChoice).classList.remove('red-glow'), 300);   
 }
 
-function draw(){
-    console.log("DRAW")
+function draw(userChoice, computerChoice){
+    // console.log("DRAW")
+    const smallUserWord = "(user)".fontsize(3).sub();
+    const smallCompWord = "(comp)".fontsize(3).sub();
+    const userChoice_div = document.getElementById(userChoice);
+    result_p.innerHTML = `${convertToWord(userChoice)}${smallUserWord} equals ${convertToWord(computerChoice)}${smallCompWord}...It's a draw!`;
+    userChoice_div.classList.add('gray-glow'); 
+    setTimeout(() => document.getElementById(userChoice).classList.remove('gray-glow'), 300);
 }
 // console.log(getComputerChoice());
 // And now we'll be getting the value r,p,s instead of 0,1,2 Which'll be random.
@@ -67,37 +105,37 @@ function game(userChoice){
         case "pr": //Similarly paper for user and rock for computer.
         case "sp":
             // console.log("USER WINS.");
-            win();
+            win(userChoice, computerChoice);
             break;
         // Now where user loses
         case "rp":
         case "ps":
         case "sr":
             // console.log("USER LOSES");
-            lose();
+            lose(userChoice, computerChoice);
             break;
         case "rr":
         case "pr":
         case "ss":
             // console.log("DRAW");
-            draw();
+            draw(userChoice, computerChoice);
             break;
     }
 }
 function main(){
-    rock_div.addEventListener('click', function(){
+    rock_div.addEventListener('click', () => game("r"))
         // console.log("hey you clicked on rock"); it'll show in the console that "You've clicked on rock" after inspecting and then console.
-        game("r"); // If user clicks on rock_div. I'm gonna provide an argument of rock
-    })
+        // game("r"); // If user clicks on rock_div. I'm gonna provide an argument of rock
+    
 
-    paper_div.addEventListener('click', function(){
+    paper_div.addEventListener('click', () => game("p"))
         // console.log("hey you clicked on paper");
-        game("p");
-    })
+        // game("p");
+            
 
-    scissors_div.addEventListener('click', function(){
+    scissors_div.addEventListener('click', () => game("s"))
         // console.log("hey you clicked on scissors");
-        game("s");
-    })
+        // game("s");
+    
 }
 main();
